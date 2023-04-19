147417610a883b77d6c107a4f6fba1af1aff09a4
Ede Zaborszky <124446749+ezaborszky@users.noreply.github.com>
2023-04-19 15:10:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const imageInput = document.getElementById("picture");
 const text = document.getElementById("text");
-//const sliderOne = document.getElementById("myRange")
+const sliderOne = document.getElementById("myRange")
 const createButton = document.getElementById("download-button")
 const fontSizeSlider = document.getElementById("font-size")
 const sliderTwo = document.getElementById("myRange2")
-//const rotateSlider = document.getElementById("rotate")
+const rotateSlider = document.getElementById("rotate")
 const opacitySlider = document.getElementById("opacity")
-// const fontSize = document.getElementById("font-size-input").value;
+const fontSize = document.getElementById("font-size-input").value;
  
-// When a new image is selected, load it into the canvas
+//When a new image is selected, load it into the canvas
 
 
 const WaterMark = () => {
@@ -31,20 +31,20 @@ const WaterMark = () => {
   // Draw the image on the canvas
   ctx.drawImage(image, 0, 0);
       
-    //   fontSize = image.height / 15;
+    fontSize = image.height / 15;
       // Set the text style
-      //let rotate = rotateSlider.value;
+      let rotate = rotateSlider.value;
       let opacity = opacitySlider.value/10;
 
       ctx.font = `${fontSize}px Arial`;
       ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
-      ctx.rotate(45 * Math.PI / 180);
+      ctx.rotate(rotate * Math.PI / 180);
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
       
       // Draw the repeating text watermark
        // Draw the repeating text watermark
-       //let sliderOneValue = sliderOne.value;
+       let sliderOneValue = sliderOne.value;
        let sliderTwoValue = sliderTwo.value;
        const xStep = canvas.width / (canvas.width/canvas.height * sliderTwoValue);
        const yStep = canvas.height / (canvas.width/canvas.height * sliderTwoValue);
@@ -56,7 +56,7 @@ const WaterMark = () => {
       
       for (let x = xStart; x < xEnd; x += xStep) {
 
-        // xStart = -canvas.width / (6 + Math.floor(Math.random() * 5));
+        //xStart = -canvas.width / (6 + Math.floor(Math.random() * 5));
         for (let y = yStart; y < yEnd; y += yStep) {
           ctx.fillText(text.value, x, y);
           ctx.strokeStyle = "black";
@@ -75,9 +75,9 @@ const WaterMark = () => {
 
  
 
-//sliderOne.addEventListener("change", function() {
-//    WaterMark();
-//})
+sliderOne.addEventListener("change", function() {
+    WaterMark();
+})
 
 fontSizeSlider.addEventListener("change", function() {
     WaterMark();
@@ -95,9 +95,9 @@ text.addEventListener("input", function() {
     WaterMark();
 })
 
-//rotateSlider.addEventListener("change", function() {
-//    WaterMark();
-//})
+rotateSlider.addEventListener("change", function() {
+    WaterMark();
+})
 
 opacitySlider.addEventListener("change", function() {
     WaterMark();
